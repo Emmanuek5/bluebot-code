d83b9ce2eceab02dd2b1e0ce122699176067f008
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-27 19:51:44 +0100

    Update and store new economy files

---
+++ src/commands/economy/balance.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+const ecoSChema = require("../../models/economy.js");
+const { Colors } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("balance").setDescription("Check Your Balance"),
+  async execute(interaction) {
+    const { user, guild } = interaction;
+
+    let Data = await ecoSChema.findOne({ Guild: guild.id, User: user.id });
+    console.log(Data);
+
+    if(!Data) {
+      return interaction.reply({ content: "You dont have an economy profile yet", ephemeral: true })
+    }
+    const wallet = Math.floor(Data.Wallet);
+    const bank = Math.floor(Data.Bank);
+    const total = wallet + bank;
+    const embed = new EmbedBuilder()
+    .setTitle("Economy")
+    .setColor(Colors.Blue)
+    .setDescription(`**Wallet**: ${wallet}\n**Bank**: ${bank}\n**Total**: ${total}  `)
+    .setTimestamp()
+    .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}` });
+    interaction.reply({ embeds: [embed] });
+
+  }
+}
+
+    
---
+++ src/commands/economy/beg.js
@@ -0,0 +1,74 @@
+const {SlashCommandBuilder, EmbedBuilder} = require('@discordjs/builders');
+const ecoSChema = require("../../models/economy.js"); 
+const { Colors } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('beg')
+        .setDescription('Beg For Money'),
+        async  execute(interaction) { 
+            const {user, guild} = interaction
+
+            let Data = await ecoSChema.findOne({ Guild: guild.id,User: user.id}) 
+            console.log(Data);
+
+            const negative= Math.round( Math.random() * -300) -10
+            const positive= Math.round( Math.random() * 300) +10
+
+            const posN = [negative,positive];
+
+            const amount = posN[Math.floor(Math.random() * posN.length)]; 
+
+            if (!amount) return interaction.reply({content: 'No Money For You Today', ephemeral: true})
+
+            if (Data) {
+                Data.Wallet += amount;
+                Data.save();
+            }
+
+            if(amount > 0) {
+
+                const positiveChoices = [
+                    "You Got Money!",
+                    "Mr Beast Donated To You",
+                    "Free Money For You",
+                    "You Got Some Money",
+                    "Today is your lucky day",
+                    "Somebody gave you money"
+                ]
+
+                const positiveChoice = positiveChoices[Math.floor(Math.random() * positiveChoices.length)];
+
+                const embed= new EmbedBuilder()
+                .setTitle("Beggar")
+                .setColor(Colors.Blue)
+                .setDescription(`${positiveChoice}\n\nYou Got ${amount} Coins`)
+
+            await  interaction.reply({embeds: [embed]})
+            
+            }else {
+                 const negativeChoices = [
+                    "CLose, But No Cigar", 
+                    "No Money Today",
+                    "Try Again Next Time ",
+                    "Dont Worry, You Will Get It",
+                    "You Lost Some Money",
+                    "You Lost Some Money"
+                ]
+
+                const negativeChoice = negativeChoices[Math.floor(Math.random() * negativeChoices.length)];
+
+                const embed= new EmbedBuilder()
+                .setTitle("Beggar")
+                .setColor(Colors.Red)
+                .setDescription(`${negativeChoice}\n\nYou Lost ${amount} Coins`)
+
+            await  interaction.reply({embeds: [embed]})
+            
+            
+            }
+        
+
+        }
+    }
