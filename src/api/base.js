ef3099fd7f619a5bc4c5b28503e37f959162787d
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-27 23:02:15 +0100

    update

--- src/api/base.js
+++ src/api/base.js
@@ -4,7 +4,7 @@
 class Api  {
     constructor(client, client_secret, app){
         this.url = "https://discord.com/api/v/9/";
-        this.client_id = client.user.id;
+        this.client_id = process.env.CLIENT_ID;
         this.client_secret = client_secret;
         this.client = client;
         this.app= app;
