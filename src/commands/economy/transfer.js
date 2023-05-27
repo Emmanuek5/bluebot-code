79c4fc0ebb509abc5343f70f7b9a279929346d0f
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-27 20:26:01 +0100

    update

--- src/commands/economy/transfer.js
+++ src/commands/economy/transfer.js
@@ -3,31 +3,62 @@ const ecoSChema = require("../../models/economy.js");
 const { Colors } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("transfer").setDescription("Transfer to your back")
-    .addNumberOption((option) => option.setName("amount").setDescription("Amount to transfer").setRequired(true)),
+  data: new SlashCommandBuilder().setName("transfer").setDescription("Transfer to your bank or a user")
+  .addSubcommand((subcommand) => subcommand.setName("user").setDescription("Transfer to a user").addUserOption((option) => option.setName("user").setDescription("User to transfer").setRequired(true))
+    .addNumberOption((option) => option.setName("amount").setDescription("Amount to transfer").setRequired(true)))
+    .addSubcommand((subcommand) => subcommand.setName("bank").setDescription("Transfer to your bank").addNumberOption((option) => option.setName("amount").setDescription("Amount to transfer").setRequired(true)))
+    ,
+   
   async execute(interaction) {
     const { user, guild } = interaction;
+    const command = interaction.options.getSubcommand();
 
     let Data = await ecoSChema.findOne({ Guild: guild.id, User: user.id });
     console.log(Data);
    if (!Data) return interaction.reply({ content: "You don't have an Economy Account", ephemeral: true })
 
-    const amount = interaction.options.getNumber("amount");
-    if (amount < 1) return interaction.reply({ content: "You can't transfer less than $1", ephemeral: true });
-    if (Data.Wallet < amount) return interaction.reply({ content: "You don't have enough money", ephemeral: true });
+    if (command === "user") {
+      const target = interaction.options.getUser("user");
+      const amount = interaction.options.getNumber("amount");
 
-    Data.Wallet -= amount;
-    Data.Bank += amount;
-    Data.save();
+      if (amount > Data.Wallet) return interaction.reply({ content: "You don't have enough money", ephemeral: true })
 
-    const embed = new EmbedBuilder()
-      .setTitle("Transfer")
-      .setColor(Colors.Blue)
+      const targetData = await ecoSChema.findOne({ Guild: guild.id, User: target.id });
+      if (!targetData) return interaction.reply({ content: "This user doesn't have an Economy Account", ephemeral: true })
+
+      Data.Wallet -= amount;
+      targetData.Wallet += amount;
+
+      await Data.save();
+      await targetData.save();
+
+      const embed = new EmbedBuilder()
+      .setColor(Colors.Green)
+      .setDescription(`You transfered ${amount} to ${target}`)
+
+      interaction.reply({ embeds: [embed] })
+    } 
+
+    if (command === "bank") {
+      const amount = interaction.options.getNumber("amount");
+
+      if (amount > Data.Wallet) return interaction.reply({ content: "You don't have enough money", ephemeral: true })
+
+      Data.Wallet -= amount;
+      Data.Bank += amount;
+
+      await Data.save();
+
+      const embed = new EmbedBuilder()
+      .setColor(Colors.Green)
       .setDescription(`You transfered **${amount}** to your bank`)
-      .setTimestamp(); 
 
-await  interaction.reply({ embeds: [embed] });
-
+      interaction.reply({ embeds: [embed] })
+    } 
+        
+    
+        
+    
 
 
 
