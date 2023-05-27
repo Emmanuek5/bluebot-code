13817063c9f2fb8bcbd9bd0327c7d80234a4591e
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-27 23:15:14 +0100

    Base.js

--- src/api/base.js
+++ src/api/base.js
@@ -1,30 +1,24 @@
+class Api {
+  constructor(client, client_secret, app) {
+    this.url = "https://discord.com/api/v/9/";
+    this.client_id = process.env.CLIENT_ID;
+    this.client_secret = client_secret;
+    this.client = client;
+    this.app = app;
+  }
 
-
-
-class Api  {
-    constructor(client, client_secret, app){
-        this.url = "https://discord.com/api/v/9/";
-        this.client_id = process.env.CLIENT_ID;
-        this.client_secret = client_secret;
-        this.client = client;
-        this.app= app;
-    }
-
-    async start(){
-        this.app.post("/api/v1/auth/create", async (req, res) => {
-            const { Authentication } = require("./Authentication/Auth");
-            const id = req.body.id;
-            const token = new Authentication().create(id).then(token => {
-                (token => {
-            console.log(id,token);
-            res.send(token).status(200)
-        })
-            })
-        })
-
-
-    }
-
+  async start() {
+    this.app.post("/api/v1/auth/create", async (req, res) => {
+      const { Authentication } = require("./Authentication/Auth");
+      const id = req.body.id;
+      const token = new Authentication().create(id).then(token => {
+      
+          console.log(id, token);
+          res.send(token).status(200);
+        });
+      
+    });
+  }
 }
 
-module.exports = {Api};
+module.exports = { Api };
