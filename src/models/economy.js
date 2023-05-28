e60a53b968fb105cff15a3786a3844b3cc1944c7
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-28 14:08:04 +0100

    Updated Base.js and Updated econmy Mangement

--- src/models/economy.js
+++ src/models/economy.js
@@ -1,10 +1,23 @@
 const mongoose  = require('mongoose');
 
 const economySChema = new mongoose.Schema({
-    Guild: String,
-    User: String,
-    Bank: String,
-    Wallet: Number
+    Guild: {
+        type: String,
+        required: true
+    },
+    User: {
+        type: String,
+        required: true
+    
+    },
+    Bank: {
+        type: Number,
+        default: 0
+    },
+    Wallet: {
+        type: Number,
+        default: 0
+    }
 })
 
 module.exports = mongoose.model("Economy", economySChema);
