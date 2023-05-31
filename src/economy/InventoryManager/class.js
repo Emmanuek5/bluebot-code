cf01eeb4a910486a9a0cffeff7bf5e7c1c5e5e09
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-05-31 18:59:52 +0100

    Update

--- src/economy/InventoryManager/class.js
+++ src/economy/InventoryManager/class.js
@@ -130,13 +130,14 @@ class InventorySystem {
   }
 
   // Function to save inventory data to JSON file
-  saveInventory() {
-    this.savetoPasteBin();
+  async saveInventory() {
+   
     fs.writeFile("inventory.json", JSON.stringify(this.inventoryData), error => {
       if (error) {
         console.log("Error saving inventory file:", error);
       }
     });
+     await this.savetoPasteBin();
   }
 
   // Function to generate a unique item ID
