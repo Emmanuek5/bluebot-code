23de561d31f9ee4daebd671aed6b6ecb8fa13c1b
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-06-27 18:25:10 +0100

    Update

--- src/commands/other/qrcode.js
+++ src/commands/other/qrcode.js
@@ -25,31 +25,36 @@ module.exports = {
         )
     ),
   async execute(interaction, client) {
+    await interaction.deferReply();
     const command = interaction.options.getSubcommand();
     try {
-        if (command === "generate") {
-          const text = interaction.options.getString("text");
-          const image = await generateQRCode(text);
-          const attachment = new AttachmentBuilder(image, { name: "qrcode.png" });
-          const embed = new EmbedBuilder()
-            .setTitle("QR Code Generator")
-            .setColor("Green")
-            .setImage("attachment://qrcode.png")
-            .setFooter({ text: "Made By The Obsidian Group" });
-          await interaction.reply({ files: [attachment], embeds: [embed] });
-        } else if (command === "read") {
-          const attachment = interaction.options.getAttachment("attachment");
-          const text = await readQRCode(attachment.url);
-          const embed = new EmbedBuilder()
-            .setTitle("QR Code Reader")
-            .setColor("Green")
-            .setDescription("The Qr Code Text is" + text)
-            .setFooter({ text: "Made By The Obsidian Group" });
-          await interaction.reply({ embeds: [embed] });
-        }
+      if (command === "generate") {
+        const text = interaction.options.getString("text");
+        const image = await generateQRCode(text);
+        const attachment = new AttachmentBuilder(image, { name: "qrcode.png" });
+        const embed = new EmbedBuilder()
+          .setTitle("QR Code Generator")
+          .setColor("Green")
+          .setImage("attachment://qrcode.png")
+          .setFooter({ text: "Made By The Obsidian Group" });
+        await interaction.editReply({ files: [attachment], embeds: [embed] });
+      } else if (command === "read") {
+        const attachment = interaction.options.getAttachment("attachment");
+        const text = await readQRCode(attachment.url);
+        const embed = new EmbedBuilder()
+          .setTitle("QR Code Reader")
+          .setColor("Green")
+          .setDescription("The Qr Code Text is" + text)
+          .setFooter({ text: "Made By The Obsidian Group" });
+        await interaction.editReply({ embeds: [embed] });
+      }
     } catch (error) {
-        console.log(error);
-        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
-        }
+      console.log(error);
+      await interaction.editReply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+      e;
+    }
   },
 };
