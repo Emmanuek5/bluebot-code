eecd83f9de28d12f0164dd6ec44b23183288231b
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-06-28 16:48:04 +0100

    Update AiHandler.js

--- src/tests/findSwearWords.test.js
+++ src/tests/findSwearWords.test.js
@@ -1,20 +1,20 @@
-const { findSwearWords } = require("../utils/swearfinder");
+const { findSwearWords, findSwearWordsAI } = require("../utils/swearfinder");
 
 describe("findSwearWords", () => {
   it("should return false when message doesn't contain any swear words", () => {
     const message = "This is a clean message";
-    const result = findSwearWords(message);
+    const result = findSwearWordsAI(message);
     expect(result).toBe(false);
   });
   it("should return true when message contains a swear word", () => {
     const message = "This message contains a swear word: damn";
-    const result = findSwearWords(message);
+    const result = findSwearWordsAI(message);
     expect(result).toBe(true);
   });
 
   it("should return true and the found swear word when message contains multiple swear words", () => {
     const message = "This message contains multiple swear words: damn and shit";
-    const result = findSwearWords(message);
+    const result = findSwearWordsAI(message);
     expect(result).toEqual([true, "damn"]);
   });
 });
