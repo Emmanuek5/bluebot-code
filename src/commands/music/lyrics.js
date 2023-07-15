aa3ce2cfa7621eda4d2229824d40a582f139ecb3
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-07-15 15:04:26 +0100

    Update

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
 const { Colors } = require("discord.js");
-const { getLyrics, getSong } = require("genius-lyrics-api");
+const { getThumbnail, getLyrics } = require("@fantox01/lyrics-scraper");
 const apiKey = "HtrnVyrrhWx762NVDY6eE2TYO3einsghCvqJlLlXZjt7kIEg-R3jFoCtuK0jTwn_";
 module.exports = {
   data: new SlashCommandBuilder().setName("lyrics").setDescription("Get lyrics for a song"),
@@ -12,23 +12,19 @@ module.exports = {
 
     if (player.queue.current) {
       const { title, author, thumbnail } = player.queue.current;
-      const options = {
-        apiKey,
-        title: title,
-        artist: author,
-        optimizeQuery: true,
-      };
-
-      getLyrics(options).then(lyrics => {
-        const embed = new EmbedBuilder()
-          .setTitle(title)
-          .setAuthor({ name: author, iconURL: thumbnail })
-          .setDescription(lyrics)
-          .setColor(Colors.Blue)
-          .setTimestamp();
-
-        interaction.reply({ embeds: [embed] });
-      });
+      const lyrics = await getLyrics(title.replace("(Official Music Video)", ""));
+      if (!lyrics) {
+        interaction.reply("No lyrics found for this song");
+        return;
+      }
+      const embed = new EmbedBuilder()
+        .setColor(Colors.Blue)
+        .setTitle(title)
+        .setAuthor({ name: author, iconURL: thumbnail })
+        .setDescription(lyrics)
+        .setThumbnail(thumbnail)
+        .setFooter({ text: `Requested by ${user.username}` });
+      interaction.reply({ embeds: [embed] });
     }
   },
 };
