022440c3cedeb893659074f5c95ad5b8e3aa47f4
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-07-17 20:23:25 +0100

    Update to the server

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT;
-const session = require('express-session');
-const mongoose = require('mongoose');
+const session = require("express-session");
+const mongoose = require("mongoose");
 const url = process.env.URL;
-const passport = require('passport');
-const flash = require('express-flash');
-const LocalStrategy = require('passport-local').Strategy;
-const localUser = require('./src/models/localuser');
-const bcrypt = require('bcrypt');
-const path = require('path');
-const { sleep } = require('./src/functions/functions');
-var favicon = require('serve-favicon');
-const http = require('http');
+const passport = require("passport");
+const flash = require("express-flash");
+const LocalStrategy = require("passport-local").Strategy;
+const localUser = require("./src/models/localuser");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const { sleep } = require("./src/functions/functions");
+var favicon = require("serve-favicon");
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require('ws');
+const { Server } = require("ws");
 const wss = new Server({
   server: server,
 });
 
 app.use(express.urlencoded({ extended: false }));
-app.set('view-engine', 'ejs');
+app.set("view-engine", "ejs");
 // import the passport module
 // import the config module
 //connect to database
 
-app.set('views', path.join(__dirname, 'src/views'));
+app.set("views", path.join(__dirname, "src/views"));
 
-require('./src/functions/passport-discord');
+require("./src/functions/passport-discord");
 
 app.use(
   session({
@@ -50,35 +50,35 @@ app.use(flash());
 
 // set up flash messages
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
   next();
 });
 
-app.use('/favicon.ico', express.static('src/public/images/favicon.ico'));
-app.use(express.static(path.join(__dirname, 'src/public')));
-const webRoutes = require('./src/routes/web');
-app.use('/', webRoutes);
+app.use("/favicon.ico", express.static("src/public/images/favicon.ico"));
+app.use(express.static(path.join(__dirname, "src/public")));
+const webRoutes = require("./src/routes/web");
+app.use("/", webRoutes);
 
 //const authRoutes = require('./src/routes/auth');
 //app.use('/api/v1/auth', authRoutes);
-const apiRoutes = require('./src/routes/apisockets');
-app.use('/api/v1', apiRoutes);
+const apiRoutes = require("./src/routes/apisockets");
+app.use("/api/v1", apiRoutes);
 
-const dashboardRoutes = require('./src/routes/dashboard');
-app.use('/dashboard', dashboardRoutes);
+const dashboardRoutes = require("./src/routes/dashboard");
+app.use("/dashboard", dashboardRoutes);
 
-const loginRoutes = require('./src/routes/login');
-app.use('/login', loginRoutes);
+const loginRoutes = require("./src/routes/login");
+app.use("/login", loginRoutes);
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('Received Message' + message);
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    console.log("Received Message" + message);
   });
 });
-
+console.log(app.routes);
 app.listen(port, () => {});
 
 module.exports = {
-  app : app
-}
+  app: app,
+};
