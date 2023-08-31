89e37e13681bdace03b3b7a8e479b5bfa7f51e96
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-08-31 12:03:51 +0100

    Update The APi

--- src/api/base.js
+++ src/api/base.js
@@ -38,45 +38,58 @@ class Api {
    * @return {void}
    */
   async start() {
-    this.app.all("/api/v1/*",(req,res)=>{
-      
-    })
-    for (const route in this.routes) {
-      const { method } = this.routes[route];
-      this.app[method.toLowerCase()](route, async (req, res) => {
-        try {
-          const { Authentication } = require("./Authentication/Auth");
-          switch (route) {
-            case "/api/v1/auth/create":
-              // Route logic for creating authentication tokens
+  app.all("/api/v1/*",this.handler(req,res));
+  for (let i = 0; i < this.routes.length; i++) {
+    console.log("Registering Route:"+ this.routes[i])
+  }   
+  }
 
+
+
+async handler(req,res) {
+ 
+      try {
+        const { Authentication } = require("./Authentication/Auth");
+        const path = req.path
+        const method = req.method
+        switch (path) {
+          case "/api/v1/auth/create":
+            // Route logic for creating authentication tokens
+            const id = req.body.id;
+            let token = new Authentication().create(id).then(token => {
+              console.log(id, token);
+              res.send(token).status(200);
+            });
+            token = "";
+            break;
+
+          case "/api/v1/user":
+       if (this.routes["/api/v1/user"].method == method) {
+             // Route logic for checking user information
+
+            const tokenraw = req.headers.authorization;
+            console.log(tokenraw);
+            token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
+            console.log(token);
+            const validate = new Authentication().validate(token).then(result => {
+              if (!result) return res.send("Please provide a valid token").status(401);
               const id = req.body.id;
-              let token = new Authentication().create(id).then(token => {
-                console.log(id, token);
-                res.send(token).status(200);
-              });
-              token = "";
-              break;
+              if (!id) return res.send("Please provide a valid id").status(401);
+              const user = this.client.users.cache.get(id);
+              res.send(user).status(200);
+            });
+            token = "";
+            break;
+       }else{
 
-            case "/api/v1/user":
-              // Route logic for checking user information
-
-              const tokenraw = req.headers.authorization;
-              console.log(tokenraw);
-              token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
-              console.log(token);
-              const validate = new Authentication().validate(token).then(result => {
-                if (!result) return res.send("Please provide a valid token").status(401);
-                const id = req.body.id;
-                if (!id) return res.send("Please provide a valid id").status(401);
-                const user = this.client.users.cache.get(id);
-                res.send(user).status(200);
-              });
-              token = "";
-              break;
-
-            case "/api/v1/user/economy":
-              // Route logic for retrieving user economy
+               res.send(
+                 "Usage: " + this.routes["/api/v1/user"].method.toUpperCase + ": " + path
+               );
+        
+       }
+          case "/api/v1/user/economy":
+            // Route logic for retrieving user economy
+          if(this.routes["/api/v1/user/economy"].method == method){
               try {
                 const ecoSchema = require("../models/economy");
                 const tokenRaw = req.headers.authorization;
@@ -117,94 +130,94 @@ class Api {
                 console.log(error);
                 res.send("An error occurred while retrieving the economy").status(500);
               }
-              break;
-
-            case "/api/v1/user/xp":
-              // Route logic for retrieving user XP
-              try {
-                const Levels = require("discord.js-leveling");
-                const tokenraw = req.headers.authorization;
-                const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
-                const validate = new Authentication().validate(token).then(result => {
-                  if (!result) return res.send("Please provide a valid token").status(401);
-                  const id = req.body.id;
-                  const guildid = req.body.guildid;
-                  if (!id) return res.send("Please provide a valid id").status(401);
-                  if (!guildid) res.send("Please provide a valid guildid").status(401);
-                  Levels.fetch(id, guildid).then(xp => {
-                    res.send(xp).status(200);
-                  });
-                });
-              } catch (error) {
-                console.log(error);
-                res.send("An error occurred while retrieving the xp").status(500);
-              }
-              break;
-
-            case "/api/v1/guilds":
-
-          
-              // Route logic for retrieving bot's guilds
-              try {
-                const tokenraw = req.headers.authorization;
-                const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
-                const validate = new Authentication().validate(token).then(result => {
-                  if (!result) return res.send("Please provide a valid token").status(401);
-                  const guilds = [];
-                  this.client.guilds.cache.forEach(guild => {
-                    const data = {
-                      id: guild.id,
-                      name: guild.name,
-                      icon: guild.iconURL(),
-                      membercount: guild.memberCount,
-                    };
-                    guilds.push(data);
-                  });
-                  res.send(guilds).status(200);
-                });
-              } catch (error) {
-                console.log(error);
-                res.send("An error occurred while retrieving the guilds").status(500);
-              }
-              break;
-
-            case "/api/v1/guild/invite":
-              // Route logic for creating guild invites
-              try {
-                const tokenraw = req.headers.authorization;
-                const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
-                const validate = new Authentication().validate(token).then(async result => {
-                  if (!result) return res.send("Please provide a valid token").status(401);
-                  const id = req.body.id;
-                  if (!id) return res.send("Please provide a valid id").status(401);
-                  const guild = this.client.guilds.cache.get(id);
-
-                  const invite = await guild.invites.create(guild.systemChannelId, {
-                    unique: true,
-                    maxAge: 0,
-                  });
-                  res.send(invite).status(200);
-                });
-              } catch (error) {
-                console.log(error);
-                res.send("An error occurred while creating the invite").status(500);
-              }
-              break;
-
-            // Add more cases for other routes if needed
-
-            default:
-              // Return 404 for unknown routes
-              res.status(404).send("Not Found");
-              break;
+          }else{
+            res.send("Usage: "+this.routes["/api/v1/user/economy"].method.toUpperCase + ": "+ path)
           }
-        } catch (error) {
-          console.log(error);
-          res.status(500).send("Internal Server Error");
+            break;
+
+          case "/api/v1/user/xp":
+            // Route logic for retrieving user XP
+            try {
+              const Levels = require("discord.js-leveling");
+              const tokenraw = req.headers.authorization;
+              const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
+              const validate = new Authentication().validate(token).then(result => {
+                if (!result) return res.send("Please provide a valid token").status(401);
+                const id = req.body.id;
+                const guildid = req.body.guildid;
+                if (!id) return res.send("Please provide a valid id").status(401);
+                if (!guildid) res.send("Please provide a valid guildid").status(401);
+                Levels.fetch(id, guildid).then(xp => {
+                  res.send(xp).status(200);
+                });
+              });
+            } catch (error) {
+              console.log(error);
+              res.send("An error occurred while retrieving the xp").status(500);
+            }
+            break;
+
+          case "/api/v1/guilds":
+            // Route logic for retrieving bot's guilds
+            try {
+              const tokenraw = req.headers.authorization;
+              const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
+              const validate = new Authentication().validate(token).then(result => {
+                if (!result) return res.send("Please provide a valid token").status(401);
+                const guilds = [];
+                this.client.guilds.cache.forEach(guild => {
+                  const data = {
+                    id: guild.id,
+                    name: guild.name,
+                    icon: guild.iconURL(),
+                    membercount: guild.memberCount,
+                  };
+                  guilds.push(data);
+                });
+                res.send(guilds).status(200);
+              });
+            } catch (error) {
+              console.log(error);
+              res.send("An error occurred while retrieving the guilds").status(500);
+            }
+            break;
+
+          case "/api/v1/guild/invite":
+            // Route logic for creating guild invites
+            try {
+              const tokenraw = req.headers.authorization;
+              const token = tokenraw.replace("Bearer ", "").replace("Bot ", "");
+              const validate = new Authentication().validate(token).then(async result => {
+                if (!result) return res.send("Please provide a valid token").status(401);
+                const id = req.body.id;
+                if (!id) return res.send("Please provide a valid id").status(401);
+                const guild = this.client.guilds.cache.get(id);
+
+                const invite = await guild.invites.create(guild.systemChannelId, {
+                  unique: true,
+                  maxAge: 0,
+                });
+                res.send(invite).status(200);
+              });
+            } catch (error) {
+              console.log(error);
+              res.send("An error occurred while creating the invite").status(500);
+            }
+            break;
+
+          // Add more cases for other routes if needed
+
+          default:
+            // Return 404 for unknown routes
+            res.status(404).send("Not Found");
+            break;
         }
-      });
-    }
-  }
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+      }
+}
+
 }
 
 module.exports = { Api };
--- test.js
+++ test.js
@@ -8,7 +8,7 @@ setTimeout(() => {
       Authorization: `Bearer adb612a7ee7ea9e9b69a25a3d298e96e09faa10525b4833784fe6a1781dd07cb46069018b3e001ce196327bd066043f56ff3e99b67c52cd180042ac75d91829b`,
     },
     data: {
-      server_count: process.env.SERVER_COUNT,
+      server_count: 70,
     },
   }).then(e => {
     console.log(e);
