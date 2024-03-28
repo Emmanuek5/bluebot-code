60bd10bde31643dfda5f84f5bd979f065c2a772c
Emmanuek5 <emmanuelandrew625@yahoo.com>
2024-03-28 10:37:32 +0100

    Update

--- src/models/economy.js
+++ src/models/economy.js
@@ -1,34 +1,13 @@
 const mongoose = require("mongoose");
 
-const economySChema = new mongoose.Schema({
-  Guild: {
-    type: String,
-    required: true,
-  },
-  User: {
-    type: String,
-    required: true,
-  },
-  Bank: {
-    type: Number,
-    default: 0,
-  },
-  Wallet: {
-    type: Number,
-    default: 0,
-  },
-  lastDaily: {
-    type: Date,
-    default: Date.now(),
-  },
-  lastWeekly: {
-    type: Date,
-    default: Date.now(),
-  },
-  lastMonthly: {
-    type: Date,
-    default: Date.now(),
-  },
+const economySchema = new mongoose.Schema({
+  Guild: { type: String, required: true },
+  User: { type: String, required: true },
+  Bank: { type: Number, default: 0 },
+  Wallet: { type: Number, default: 0 },
+  lastDaily: { type: Number, default: null },
+  lastWeekly: { type: Number, default: null },
+  lastMonthly: { type: Number, default: null },
 });
 
-module.exports = mongoose.model("Economy", economySChema);
+module.exports = mongoose.model("Economy", economySchema);
